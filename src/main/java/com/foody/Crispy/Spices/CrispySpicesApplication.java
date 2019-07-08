1fc43314cc9cd0f3d4c5dcc829792cc97a3084b0
Manjeet <sharma.manjeet@gmail.com>
2019-07-08 11:16:27 +0530

    Fixed

---
+++ src/main/java/com/foody/Crispy/Spices/CrispySpicesApplication.java
@@ -0,0 +1,13 @@
+package com.foody.Crispy.Spices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrispySpicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrispySpicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foody/Crispy/Spices/CrispySpicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foody.Crispy.Spices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrispySpicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
